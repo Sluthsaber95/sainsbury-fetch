45d8bc3e5a939b23b3e6074647a82df5982a33a6
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-29 21:59:34 +0000

    Feat ImagesDisplayed include additional Prop and a fallback
    Line 15 - 20 Include an additional fallback just incase the user does not find hat he/ she/it is looking for

---
+++ src/components/ImagesDisplayed/ImagesDisplayed.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ImageDisplayed from './index'
+import ImagesNoResults from './ImagesNoResults'
+import { shallow } from 'enzyme'
+import '../../../config/enzyme-test-setup'
+
+it('renders without crashing', () => {
+  const shallowWrapper = shallow(<ImageDisplayed />)
+  expect(shallowWrapper.exists()).toEqual(true)
+})
+
+it('render "ImagesNoResults" component when collection is empty', () => {
+  const shallowWrapper = shallow(<ImageDisplayed />)
+  expect(shallowWrapper.find(ImagesNoResults).length).toBe(1)
+})
--- src/components/ImagesDisplayed/index.jsx
+++ src/components/ImagesDisplayed/index.jsx
@@ -1,22 +1,34 @@
 // @flow
 import * as React from 'react'
 
-import ImageWrapper, { defaultValues } from './ImageWrapper'
+import ImageWrapper from './ImageWrapper'
+import ImagesNoResults from './ImagesNoResults'
 
 type Props = {
-  collection: $ReadOnlyArray<{ src: string, alt: string }>,
+  collection: $ReadOnlyArray<{ src: string, alt: string, key: string }>,
 }
 
 export default function ImagesDisplayed(props: Props) {
+  const { collection } = props
   return (
     <section>
-      {props.collection.map(({ alt, src }) => {
-        return <ImageWrapper src={src} alt={alt} />
-      })}
+      {collection.length > 0 && collection[0].src !== undefined ? (
+        collection.map(({ alt, src, key }) => {
+          return <ImageWrapper src={src} alt={alt} key={key} />
+        })
+      ) : (
+        <ImagesNoResults />
+      )}
     </section>
   )
 }
 
 ImagesDisplayed.defaultProps = {
-  collection: [defaultValues],
+  collection: [
+    {
+      alt: '',
+      key: '',
+      src: '',
+    },
+  ],
 }
