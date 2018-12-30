78ff70e680fe7c24318005fefa8cc36945c455d7
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-30 21:27:33 +0000

    Testing search_spec init

---
+++ cypress/integration/routes/search_spec.js
@@ -0,0 +1,12 @@
+describe('/search - As a user...', function() {
+  context('I see', () => {
+    it('An empty search field', () => {})
+    it('Within the search box - text "Search"', () => {})
+    it('Default Images (plural) below the search bar"', () => {})
+  })
+  context('When I type', () => {
+    it('0, 1 or 2 Characters - the default screen will not change (i.e. default images are still present)', () => {})
+    it('3 or more characters - a search initiates that returns me back images from my search', () => {})
+    it('Typing an unrecoginisable set of characters - should show me "Sorry, we couldn\'t find the results you were looking for" ', () => {})
+  })
+})
