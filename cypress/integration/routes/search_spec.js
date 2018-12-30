1d3503c1129c504845afe2b926f692c8a6525c1c
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-30 22:32:27 +0000

    Testing ImageDisplay contains same default images

--- cypress/integration/routes/search_spec.js
+++ cypress/integration/routes/search_spec.js
@@ -21,8 +21,37 @@ describe('/search - As a user...', () => {
     })
   })
   context('When I type', () => {
-    it('0, 1 or 2 Characters - the default screen will not change (i.e. default images are still present)', () => {})
-    it('3 or more characters - a search initiates that returns me back images from my search', () => {})
-    it('Typing an unrecoginisable set of characters - should show me "Sorry, we couldn\'t find the results you were looking for" ', () => {})
+    const alphanumVal = 'w2UpMdFy'
+    const charList = alphanumVal.split('')
+    const twoLettersList = charList.map(
+      (letter, i) => letter + charList[charList.length - (i + 1)]
+    )
+    const shortWordSet = [
+      {
+        description: '1 Character',
+        list: charList,
+      },
+      {
+        description: '2 Characters',
+        list: twoLettersList,
+      },
+    ]
+    shortWordSet.forEach(wordLength => {
+      const { list } = wordLength
+      list.forEach(word => {
+        it(`${
+          wordLength.description
+        } the default screen will not change when entering ${word}`, () => {
+          const defaultAlt = 'KSC-99pp0496'
+          cy.get('.search-bar').type(word)
+
+          cy.get('.img-display')
+            .children('.img-thumb')
+            .should('have.attr', 'alt', defaultAlt)
+        })
+      })
+    })
+    xit('3 or more characters - a search initiates that returns me back images from my search', () => {})
+    xit('Typing an unrecoginisable set of characters - should show me "Sorry, we couldn\'t find the results you were looking for" ', () => {})
   })
 })
