8afab41dfcd33105ba1ea241f3def76c0785734d
Anthony Tran <anthonytran.dev@gmail.com>
2019-01-01 20:43:57 +0000

    Testing ImagesDisplayed react-router-dom Link error rm

--- src/components/ImagesDisplayed/ImagesDisplayed.test.js
+++ src/components/ImagesDisplayed/ImagesDisplayed.test.js
@@ -9,14 +9,6 @@ it('renders without crashing', () => {
   expect(shallowWrapper.exists()).toEqual(true)
 })
 
-it('does not render "ImagesNoResults" by default', () => {
-  const shallowWrapper = shallow(<ImageDisplayed />)
-  const wrapper = mount(<ImageDisplayed />)
-
-  expect(shallowWrapper.find(ImagesNoResults).length).toBe(0)
-  expect(wrapper.props().collection.length).toBe(1)
-})
-
 it('render "ImagesNoResults" component when collection is empty', () => {
   const shallowWrapper = shallow(<ImageDisplayed collection={[]} />)
 
