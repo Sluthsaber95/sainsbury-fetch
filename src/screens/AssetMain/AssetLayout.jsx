1c288923ffc58b9382ec1c2f09b0a6c1a0559009
Anthony Tran <anthonytran.dev@gmail.com>
2019-01-01 20:20:02 +0000

    Feat AssetLayout Init

---
+++ src/screens/AssetMain/AssetLayout.jsx
@@ -0,0 +1,32 @@
+// @flow
+import React from 'react'
+
+interface Metadata {
+  description: string;
+  location: string;
+  photographer: string;
+  title: string;
+}
+
+type Props = {
+  nasa_id: string,
+  metadata: Metadata,
+}
+
+export default function AssetLayout(props: Props) {
+  const { nasa_id, metadata } = props
+  const { description, location, photographer, title } = metadata
+  console.log(metadata)
+  return (
+    <div>
+      <div>{location}</div>
+      <div>{description}</div>
+      <div>{title}</div>
+      <div>{photographer}</div>
+      <img
+        src={`http://images-assets.nasa.gov/image/${nasa_id}/${nasa_id}~medium.jpg`}
+        alt={location}
+      />
+    </div>
+  )
+}
