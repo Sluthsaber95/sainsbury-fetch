ce16d273edcbe70d324185f1e673b122129fee5b
Anthony Tran <anthonytran.dev@gmail.com>
2019-01-02 22:41:02 +0000

    Feat AssetMain passes descriptive data down to AssetLayout

--- src/components/AudioDisplayed/index.jsx
+++ src/components/AudioDisplayed/index.jsx
@@ -1,19 +1,40 @@
 // @flow
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 interface AudioDataSerialized {
   description: string;
   location: string;
-  media_type: string;
   nasa_id: string;
-  photographer: string;
 }
 
 type Props = {
-  collection: AudioDataSerialized,
+  collection: Array<AudioDataSerialized>,
 }
 
 export default function AudioDisplayed(props: Props) {
   const { collection } = props
-  return <div>{collection.photographer}</div>
+  return (
+    <section>
+      {collection.map(({ description, location, nasa_id }) => {
+        const shortenDescription = text => {
+          if (!text) {
+            return ''
+          } else if (text.length > 100) {
+            return text.slice(0, 99) + '...'
+          }
+          return text
+        }
+        const finalDescription = shortenDescription(description)
+        return (
+          <Link to={`/asset/${nasa_id}`} key={nasa_id}>
+            <article>
+              <div>{finalDescription}</div>
+              <h5>{location}</h5>
+            </article>
+          </Link>
+        )
+      })}
+    </section>
+  )
 }
