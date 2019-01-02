d993d5c04c8b379b8329fa06373e0ffa677ebe19
Anthony Tran <anthonytran.dev@gmail.com>
2019-01-02 22:45:19 +0000

    Feat AssetMain passes down data to Assetlayout

--- src/screens/AssetMain/index.jsx
+++ src/screens/AssetMain/index.jsx
@@ -5,6 +5,23 @@ import axios from 'axios'
 
 import AssetLayout from './AssetLayout'
 
+type NextPPrevS = {
+  media_type: string,
+}
+
+type JSONImg = {
+  'AVAIL:Description': string,
+  'AVAIL:Location': string,
+  'AVAIL:Photographer': string,
+  'AVAIL:Title': string,
+}
+
+type JSONAudio = {
+  'AVAIL:Description': string,
+  'AVAIL:Location': string,
+  'AVAIL:Title': string,
+}
+
 type State = {
   description: string,
   location: string,
@@ -14,10 +31,15 @@ type State = {
 
 type Props = {
   match: Object,
+  media_type: 'image' | 'audio',
 }
 
+const AssetLayoutAudio = () => <div>Asset Layout Audio Here!!</div>
+
 export default class AssetMain extends Component<Props, State> {
-  getImgData: () => void
+  getData: () => void
+  serializeAudioJSONL: (data: JSONAudio) => void
+  serializeImgJSON: (data: JSONImg) => void
   constructor(props: Props) {
     super(props)
     this.state = {
@@ -25,45 +47,96 @@ export default class AssetMain extends Component<Props, State> {
       location: '',
       photographer: '',
       title: '',
+      nasa_id: '',
     }
-    this.getImgData = this.getImgData.bind(this)
+    this.getData = this.getData.bind(this)
+    this.serializeImgJSON = this.serializeImgJSON.bind(this)
   }
-  getImgData() {
+  static getDerivedStateFromProps = (
+    nextProps: NextPPrevS,
+    prevState: NextPPrevS
+  ) => {
+    if (nextProps.media_type !== prevState.media_type) {
+      return { media_type: nextProps.media_type }
+    }
+    return {}
+  }
+  getData() {
     const nasa_id = this.props.match.params.nasa_id
-    return axios
-      .get(`https://images-assets.nasa.gov/image/${nasa_id}/metadata.json`, {})
-      .then(response => {
-        if (response.status >= 200 && response.status < 400) {
-          const description = response.data['AVAIL:Description']
-          const location = response.data['AVAIL:Location']
-          const photographer =
-            response.data['AVAIL:Photographer'] === ''
-              ? 'Photography Unknown'
-              : response.data['AVAIL:Photographer']
-          const title = /^KSC/.test(response.data['AVAIL:Title'])
-            ? '[ No Title]'
-            : response.data['AVAIL:Title']
+    const media_type = this.props.media_type
+    try {
+      axios
+        .get(
+          `https://images-assets.nasa.gov/${media_type}/${nasa_id}/metadata.json`,
+          {}
+        )
+        .then(response => {
+          const { data, status } = response
+          if (status >= 200 && status < 400) {
+            return media_type === 'image'
+              ? this.serializeImgJSON(data)
+              : this.serializeAudioJSON(data)
+          } else {
+            return new Error('')
+          }
+        })
+    } catch (e) {
+      console.log(e)
+    } finally {
+      axios
+        .get(
+          `https://images-assets.nasa.gov/audio/${nasa_id}/metadata.json`,
+          {}
+        )
+        .then(response => {
+          const { data, status } = response
+          if (status >= 200 && status < 400) {
+            return media_type === 'image'
+              ? this.serializeImgJSON(data)
+              : this.serializeAudioJSON(data)
+          } else {
+            return new Error('asdf')
+          }
+        })
+    }
+  }
+  serializeImgJSON(data: JSONImg) {
+    const description = data['AVAIL:Description']
+    const location = data['AVAIL:Location']
+    const photographer =
+      data['AVAIL:Photographer'] === ''
+        ? 'Photographer Unknown'
+        : data['AVAIL:Photographer']
+    const title = /^KSC/.test(data['AVAIL:Title'])
+      ? '[ No Title]'
+      : data['AVAIL:Title']
 
-          this.setState(() => {
-            return {
-              description,
-              location,
-              photographer,
-              title,
-            }
-          })
-        }
-        return
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    this.setState(() => {
+      return {
+        description,
+        location,
+        photographer,
+        title,
+      }
+    })
+  }
+  serializeAudioJSON(data: JSONAudio) {
+    const description = data['AVAIL:Description']
+    const location = data['AVAIL:Location']
+    const title = data['AVAIL:Title']
+    this.setState(() => {
+      return {
+        description,
+        location,
+        title,
+      }
+    })
   }
   componentDidMount() {
-    this.getImgData()
+    this.getData()
   }
   render() {
-    const { match } = this.props
+    const { match, media_type } = this.props
     const { params, url } = match
     return (
       <section>
@@ -71,6 +144,7 @@ export default class AssetMain extends Component<Props, State> {
           path={`${url}`}
           render={() => (
             <AssetLayout
+              media_type={media_type}
               metadata={{ ...this.state }}
               nasa_id={params.nasa_id}
             />
